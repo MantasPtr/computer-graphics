7b13ed05ff6e1d6255aa33b1a1fc3903d2ce80d4
Mantas <man.petrikas@gmail.com>
2018-12-10 21:46:41 +0200

    cleanup

--- homework/threejs/camera/camera.js
+++ homework/threejs/camera/camera.js
@@ -6,8 +6,6 @@ window.onload = init
 
 const logs = (a) => { console.log(a); return a; }
 
-
-
 function resolveCamera(cameraString){
     switch(cameraString) {
         case "camera 1" :
@@ -50,36 +48,33 @@ function init() {
     cameraMesh.position.x = -2.687;
     cameraMesh.position.y = 20;
     cameraMesh.position.z = -5.798
-    scene.add( new THREE.AxesHelper( 50 ))
-    
+
     scene.add(cameraMesh)
-    
+
     camera1.position.x = -40;
     camera1.position.y = 100;
     camera1.position.z = -80;
     camera1.lookAt(scene.position);
-    
+
     const sinA = 10/30
     const cosA = Math.sqrt(800)/30
     camera2.position.x = (cosA*30)/Math.sqrt(2);
     camera2.position.y = 10 + 30*sinA;
     camera2.position.z = (cosA*30)/Math.sqrt(2);
     camera2.lookAt(0,10,0)
-    
-    
+
     camera3.position.x = -2.687;
     camera3.position.y = 20;
     camera3.position.z = -5.798;
-   
+
     const cameraHelper = new THREE.CameraHelper(camera3);
-  
+
     scene.add(cameraHelper)
     camera3.lookAt(new THREE.Vector3(-10,10,-10))
-  
 
     document.querySelector("#WebGL-output").append(renderer.domElement);
     animate()
-    
+
     function animate(time) {
         const positions = calcAnimations(time)
         update1stCamera()
@@ -117,24 +112,18 @@ function init() {
         cameraHelper.update()
     }
     function update3rdCamera (){
-        // camera3.up = new THREE.Vector3(-1,0,0)
-        // cameraMesh.up = new THREE.Vector3(-1,0,0)
         camera3.lookAt(keg.getWorldPosition())
         const xDiff = Math.abs(keg.getWorldPosition().x - camera3.position.x)
         const zDiff = Math.abs(keg.getWorldPosition().z - camera3.position.z)
         const diff = Math.sqrt(Math.pow(xDiff,2) + Math.pow(zDiff,2))
         const threshold = 5
-        if (diff < threshold) {
-           
+        if (diff < threshold) {   
             const angle = (diff/threshold)
-            // logs([-Math.cos(angle),Math.sin(angle)])
-            cameraMesh.up = new THREE.Vector3(0,angle,angle-1)
             camera3.up = new THREE.Vector3(0,angle,angle-1)
         } else {
-            cameraMesh.up = new THREE.Vector3(0,1,0)
-            camera3.up = new THREE.Vector3(0,1,0)
+            camera3.up = new THREE.Vector3(0,1,0)   
         }
-        // camera3.updateProjectionMatrix()
+        cameraMesh.up = camera3.up
         cameraMesh.visible = controls.displayThirdCamera
     }
 }
